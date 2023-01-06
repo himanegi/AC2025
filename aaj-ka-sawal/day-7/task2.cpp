6f0d957e91572002de679bbed429fd8113fdb7b0
himanegi <51815259+himanegi@users.noreply.github.com>
2023-01-07 01:55:42 +0530

    Add task2

---
+++ aaj-ka-sawal/day-7/task2.cpp
@@ -0,0 +1,38 @@
+// Link:https://practice.geeksforgeeks.org/problems/missing-number4257/1
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int missingNumber(int a[], int n);
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int i = 0, n;
+        cin >> n;
+        int a[n + 5];
+        for (i = 0; i < n - 1; i++)
+            cin >> a[i];
+
+        cout << missingNumber(a, n) << endl;
+    }
+}
+// } Driver Code Ends
+
+int missingNumber(int A[], int N)
+{
+    // Your code goes here
+    int s = N * (N + 1) / 2, sum = 0;
+    for (int i = 0; i < N - 1; i++)
+    {
+        sum += A[i];
+    }
+    return s - sum;
+}
+
+// Time Complexity : O(N)
+// Space Complexity : O(1)
